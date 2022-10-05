b28722a9bdfd82c0c463862f149e8038f1a122a0
jesus.acevedo <antoniozkt@gmail.com>
2022-10-04 23:14:16 -0500

    delete-console

--- src/db/dynamodb.js
+++ src/db/dynamodb.js
@@ -1,7 +1,6 @@
 // Create service client module using ES6 syntax.
 const AWS = require("aws-sdk");
 
-console.log("test", process.env.AWS_ACCESS_KEY_ID)
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
