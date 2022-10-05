ac459b68940e9544d78731023edbc881e887a00e
jesus.acevedo <antoniozkt@gmail.com>
2022-10-04 23:12:30 -0500

    fix-env-variables-local

--- src/db/dynamodb.js
+++ src/db/dynamodb.js
@@ -1,7 +1,10 @@
 // Create service client module using ES6 syntax.
 const AWS = require("aws-sdk");
 
+console.log("test", process.env.AWS_ACCESS_KEY_ID)
 AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "us-east-2",
 });
 
